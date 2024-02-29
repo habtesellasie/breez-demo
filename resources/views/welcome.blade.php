4ef989249d2419ecb992f381bdef23d5fd298b13
Habtesellasie Fissha <havecodesoft@gmail.com>
2024-02-29 05:27:59 +0300

    git init with breez

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+</head>
+
+<body class="bg-slate-300">
+    <a href="/register">Register</a>
+    <a href="/login">Login</a>
+</body>
+
+</html>
